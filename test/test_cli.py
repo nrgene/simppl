82608230e060e1033c646dd483ed390183608769
doronst <doronst5@gmail.com>
2020-09-11 18:20:25 +0300

    move resources into example_module, and fix test

--- test/test_cli.py
+++ test/test_cli.py
@@ -30,11 +30,11 @@ class TestCli(unittest.TestCase):
 
     def test_cli_add_two_numbers_example(self):
         cli = CommandLineInterface(example_module.__main__.__file__, self.ascii_logo)
-        return_value = cli.run(['my_tool_box', 'add_two_numbers_example', '1.5', '2'])
+        return_value = cli.run(['my_tool_box', 'add_two_numbers', '1.5', '2'])
         self.assertEqual(0, return_value)
 
     def test_cli_analyze_file_pipeline_example(self):
         cli = CommandLineInterface(example_module.__main__.__file__, self.ascii_logo)
         project_dir = os.path.dirname(os.path.dirname(__file__))
-        return_value = cli.run(['my_tool_box', 'analyze_file_pipeline', f'{project_dir}/resources/analyze_file_pipeline_input.txt', 'test_outputs'])
+        return_value = cli.run(['my_tool_box', 'analyze_file_pipeline', f'{project_dir}/example_module/resources/analyze_file_pipeline_input.txt', 'test_outputs'])
         self.assertEqual(0, return_value)
