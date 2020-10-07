8e074f0af089393019f872d012f2169357bebebc
doronst <doronst5@gmail.com>
2020-10-07 13:47:37 +0300

    add readme to pypi package

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="simppl",
     author='Doron Shem-Tov',
     author_email='doronst5@gmail.com',
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     include_package_data=True,
     url="https://github.com/nrgene/simppl",
     description="simple commands execution pipeline, coupled with unified command-line-interface entry-point",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'colorama',
         'pytest'
