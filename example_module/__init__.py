47c30d7f3209895eb41113eeee763d7cf4fa167d
doronst <doronst5@gmail.com>
2020-10-14 20:54:06 +0300

    dedup ascii_logo example

--- example_module/__init__.py
+++ example_module/__init__.py
@@ -2,6 +2,7 @@ import os
 from logging.config import fileConfig
 from os import path
 
+# Uncomment these lines in your actual module for turning simple-pipeline logs on
 log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging_config.ini')
 if not path.exists(log_file_path):
     exit(f'''missing logging config file: {log_file_path}
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,41 +2,23 @@ import os
 import unittest
 
 import example_module.__main__
+from example_module.__main__ import ascii_logo
 from simppl.cli import CommandLineInterface
 
 
 class TestCli(unittest.TestCase):
-    ascii_logo = '''
-                       ____                       ___                         ,--,                                           
-                     ,'  , `.                   ,--.'|_                     ,--.'|             ,---,                         
-                  ,-+-,.' _ |                   |  | :,'   ,---.     ,---.  |  | :           ,---.'|      ,---.              
-               ,-+-. ;   , ||                   :  : ' :  '   ,'\\   '   ,'\\ :  : '           |   | :     '   ,'\\ ,--,  ,--,  
-              ,--.'|'   |  ||    .--,         .;__,'  /  /   /   | /   /   ||  ' |           :   : :    /   /   ||'. \\/ .`|  
-             |   |  ,', |  |,  /_ ./|         |  |   |  .   ; ,. :.   ; ,. :'  | |           :     |,-..   ; ,. :'  \\/  / ;  
-             |   | /  | |--', ' , ' :         :__,'| :  '   | |: :'   | |: :|  | :           |   : '  |'   | |: : \\  \\.' /   
-             |   : |  | ,  /___/ \\: |           '  : |__'   | .; :'   | .; :'  : |__         |   |  / :'   | .; :  \\  ;  ;   
-             |   : |  |/    .  \\  ' |           |  | '.'|   :    ||   :    ||  | '.'|        '   : |: ||   :    | / \\  \\  \\  
-             |   | |`-'      \\  ;   :           ;  :    ;\\   \\  /  \\   \\  / ;  :    ;        |   | '/ : \\   \\  /./__;   ;  \\ 
-             |   ;/           \\  \\  ;           |  ,   /  `----'    `----'  |  ,   /         |   :    |  `----' |   :/\\  \\ ; 
-             '---'             :  \\  \\           ---`-'                      ---`-'          /    \\  /          `---'  `--`  
-                                \\  ' ;                                                       `-'----'                        
-                                 `--`                                                                                       
-    '''
-
 
     def test_cli_no_params(self):
-        cli = CommandLineInterface(example_module.__main__.__file__, self.ascii_logo)
+        cli = CommandLineInterface(example_module.__main__.__file__, ascii_logo)
         cli.run(['my_tool_box'])
 
-
     def test_cli_add_two_numbers_example(self):
-        cli = CommandLineInterface(example_module.__main__.__file__, self.ascii_logo)
+        cli = CommandLineInterface(example_module.__main__.__file__, ascii_logo)
         return_value = cli.run(['my_tool_box', 'add_two_numbers', '1.5', '2'])
         self.assertEqual(0, return_value)
 
-
     def test_cli_analyze_file_pipeline_example(self):
-        cli = CommandLineInterface(example_module.__main__.__file__, self.ascii_logo)
+        cli = CommandLineInterface(example_module.__main__.__file__, ascii_logo)
         project_dir = os.path.dirname(os.path.dirname(__file__))
         return_value = cli.run(['my_tool_box', 'analyze_file_pipeline',
                                 f'{project_dir}/example_module/resources/analyze_file_pipeline_input.txt',
