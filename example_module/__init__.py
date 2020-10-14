b50d3c4b6e69321d0e3407b73b77414e7686d771
doronst <doronst5@gmail.com>
2020-10-14 18:34:08 +0300

    add listdir when logging.ini is not found

--- example_module/__init__.py
+++ example_module/__init__.py
@@ -1,7 +1,9 @@
+import os
 from logging.config import fileConfig
 from os import path
 
 log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging_config.ini')
 if not path.exists(log_file_path):
-    exit(f'missing logging config file: {log_file_path}')
+    exit(f'''missing logging config file: {log_file_path}
+            files in directory: {os.listdir(path.dirname(path.abspath(__file__)))}''')
 fileConfig(log_file_path)
